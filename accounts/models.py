cd436d2cbf1300c8084980fcb6714ff8c69ad838
Khabib Murtuzaaliev <khabib.murtuzaaliev@gmail.com>
2017-04-08 20:59:28 +0300

    Profile model creation

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
+from django.conf.global_settings import AUTH_USER_MODEL
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(AUTH_USER_MODEL)
+    slug = models.SlugField(allow_unicode=True, unique=True)
+
+    def __str__(self):
+        return ('%s' % self.user.username).encode('utf-8', errors='replace')
+
+    def __unicode__(self):
+        return u'{u}\'s profile'.format(u=self.user.username)
