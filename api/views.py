dd574905aa0432377d8a2eb1f0669c8ae2adaa9e
Khabib Murtuzaaliev <khabib.murtuzaaliev@gmail.com>
2017-04-08 08:28:12 +0300

    Sort by start time

--- api/views.py
+++ api/views.py
@@ -9,23 +9,23 @@ from datetime import datetime
 class CountryGeoViewSet(viewsets.ReadOnlyModelViewSet):
     pagination_class = GeoJsonPagination
     queryset = WorldBorder.objects.filter(startyear__year__lte=datetime.now().year,
-                                          endyear__year__gte=datetime.now().year).order_by('name')
+                                          endyear__year__gte=datetime.now().year).order_by('startyear')
     serializer_class = serializers.CountrySerializer
 
     def get_queryset(self):
         year = self.request.query_params.get('year', datetime.now().year)
         queryset = WorldBorder.objects.filter(startyear__year__lte=year,
-                                              endyear__year__gte=year).order_by('name')
+                                              endyear__year__gte=year).order_by('startyear')
         return queryset
 
 
 class CountryViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = WorldBorder.objects.filter(startyear__year__lte=datetime.now().year,
-                                          endyear__year__gte=datetime.now().year).order_by('name')
+                                          endyear__year__gte=datetime.now().year).order_by('startyear')
     serializer_class = serializers.CountryListSerializer
 
     def get_queryset(self):
         year = self.request.query_params.get('year', datetime.now().year)
         queryset = WorldBorder.objects.filter(startyear__year__lte=year,
-                                              endyear__year__gte=year).order_by('name')
+                                              endyear__year__gte=year).order_by('startyear')
         return queryset
