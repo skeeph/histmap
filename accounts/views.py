2d7c2ef0d8cd89b84fe3bcf8e8f1a3e19b2ae114
Khabib Murtuzaaliev <khabib.murtuzaaliev@gmail.com>
2017-04-08 21:21:38 +0300

    Profile view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
+from accounts.models import Profile
+
+
+@login_required
+def profile(request):
+    p = Profile.objects.get(user=request.user)
+
+    return render(request, 'registration/profile.html', {
+        'profile': p,
+    })
