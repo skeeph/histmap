2bd6d8439705559eec2d11a6b31a76d00d2deb3e
Khabib Murtuzaaliev <khabib.murtuzaaliev@gmail.com>
2017-04-08 08:20:49 +0300

    Api filtering

--- api/views.py
+++ api/views.py
@@ -12,8 +12,20 @@ class CountryGeoViewSet(viewsets.ReadOnlyModelViewSet):
                                           endyear__year__gte=datetime.now().year).order_by('name')
     serializer_class = serializers.CountrySerializer
 
+    def get_queryset(self):
+        year = self.request.query_params.get('year', datetime.now().year)
+        queryset = WorldBorder.objects.filter(startyear__year__lte=year,
+                                              endyear__year__gte=year).order_by('name')
+        return queryset
+
 
 class CountryViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = WorldBorder.objects.filter(startyear__year__lte=datetime.now().year,
                                           endyear__year__gte=datetime.now().year).order_by('name')
     serializer_class = serializers.CountryListSerializer
+
+    def get_queryset(self):
+        year = self.request.query_params.get('year', datetime.now().year)
+        queryset = WorldBorder.objects.filter(startyear__year__lte=year,
+                                              endyear__year__gte=year).order_by('name')
+        return queryset
