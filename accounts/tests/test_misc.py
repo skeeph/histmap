94940484963fdaadc29977ae5bce15175dd07567
Khabib Murtuzaaliev <khabib.murtuzaaliev@gmail.com>
2017-09-28 23:52:05 +0300

    Delete bad test

--- accounts/tests/test_misc.py
+++ accounts/tests/test_misc.py
@@ -10,13 +10,3 @@ class AccountsConfigTest(TestCase):
         self.assertEqual(AccountsConfig.name, 'accounts')
         self.assertEqual(apps.get_app_config('accounts').name, 'accounts')
 
-
-class ContextProcessorTest(TestCase):
-    def test_auth0(self):
-        url = '/'
-        keys = ['AUTH0_DOMAIN', 'AUTH0_CLIENT_ID', 'AUTH0_CALLBACK_URL']
-        cfg = get_config()
-        client = self.client.get(url)
-        response = str(client.content)
-        for i in keys:
-            self.assertIn(cfg[i], response)
