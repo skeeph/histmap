e2aed425e8d89901b7446ecb4fad913dc69b3253
Khabib Murtuzaaliev <khabib.murtuzaaliev@gmail.com>
2017-09-29 14:10:49 +0300

    Delete npm dep

--- geodjango/components/static.py
+++ geodjango/components/static.py
@@ -6,13 +6,6 @@ STATIC_URL = '/gstatic/'
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'npm.finders.NpmFinder'
 ]
 
 STATIC_ROOT = "gstatic"
-
-NPM_FILE_PATTERNS = {
-    'leaflet': ['dist/*'],
-    'bootstrap': ['dist/*']
-
-}
